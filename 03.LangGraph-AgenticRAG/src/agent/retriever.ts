cac654d232b7bc747ef2958495545ca9d5bb8999
sungkwang <sungkwang@wantedlab.com>
2025-09-29 22:42:01 +0900

    feat: dot-flow 로 변경

--- 03.LangGraph-AgenticRAG/src/agent/retriever.ts
+++ 03.LangGraph-AgenticRAG/src/agent/retriever.ts
@@ -3,14 +3,14 @@ import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { OpenAIEmbeddings } from "@langchain/openai";
 // import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
-import dotenv from "dotenv";
+import dotenvFlow from 'dotenv-flow';
 
-dotenv.config();
+dotenvFlow.config();
 
 const urls = [
   "https://blog.naver.com/ban__di/223783862789",
   "https://lazyyellow.tistory.com/87",
-  "https://lazyyellow.tistory.com/entry/%EC%9E%A0%EC%8B%A4-%EC%A7%81%EC%9E%A5%EC%9D%B8%EC%9D%B4-%EC%95%8C%EB%A0%A4%EC%A3%BC%EB%8A%94-%EC%9E%A0%EC%8B%A4%EA%B7%BC%EC%B2%98-%EB%A7%9B%EC%A7%91",
+  "https://funktionalflow.com/%EC%9E%A0%EC%8B%A4-%EB%A7%9B%EC%A7%91/",
 ];
 
 const docs = await Promise.all(
--- 03.LangGraph-AgenticRAG/src/index.ts
+++ 03.LangGraph-AgenticRAG/src/index.ts
@@ -3,7 +3,7 @@ import { HumanMessage } from '@langchain/core/messages';
 import { graph } from './agent/graph.ts';
 
 
-const message = "잠실 남자들이 좋아하는 점심 메뉴 추천해줘."
+const message = "점심에 파스타 잘하는 식당은?"
 
 const initialState = {
     messages: [new HumanMessage(message)],
